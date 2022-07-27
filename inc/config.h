0bce475b29658be28a5a6c18d6128d1418a7d40e
Awry Zx <zxawry@tuta.io>
2022-07-27 13:32:36 +0100

    Initiate codebase with boilerplate infinite loop
    Changes to be committed:
    	new file:   .gitignore
    	new file:   Makefile
    	new file:   inc/config.h
    	new file:   src/main.c

---
+++ inc/config.h
@@ -0,0 +1,12 @@
+// file: config.h
+// author: awry
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+// make sure CPU clock frequency is configured.
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif // F_CPU
+
+#endif // CONFIG_H_
---
+++ src/main.c
@@ -0,0 +1,12 @@
+// file: main.c
+// author: awry
+
+#include "config.h"
+
+int main(void)
+{
+	for (;;) {
+	}
+
+	return 0;
+}
