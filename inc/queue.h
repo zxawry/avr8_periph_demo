d7edb0a67cf18ea559fc75b20f581f1ab12ea815
Awry Zx <zxawry@tuta.io>
2022-09-27 13:30:03 +0100

    Update `queue` module with the latest modifications
    Changes to be committed:
    	modified:   inc/queue.h
    	modified:   src/queue.c

--- inc/queue.h
+++ inc/queue.h
@@ -7,7 +7,13 @@
 #include <stdint.h>
 
 // configure size of the queue.
-#define QUEUE_SIZE 64
+#define QUEUE_SIZE (64)
+
+// assume queue size is a power of two.
+#define QUEUE_SIZE_MASK (QUEUE_SIZE - 1)
+
+// advance indeces uniquely.
+#define ADVANCE_INDEX(i) ((i + 1) & QUEUE_SIZE_MASK)
 
 // queue type definition
 typedef struct {
@@ -21,15 +27,48 @@ typedef struct {
 void queue_init(queue_t * queue);
 
 // check if the queue is empty.
-char queue_is_empty(queue_t * queue);
+__attribute__((always_inline))
+inline char queue_is_empty(queue_t * queue);
 
 // check if the queue is full.
-char queue_is_full(queue_t * queue);
+__attribute__((always_inline))
+inline char queue_is_full(queue_t * queue);
 
 // append an item to the tail of the queue.
-void queue_enqueue(queue_t * queue, const char item);
+__attribute__((always_inline))
+inline void queue_enqueue(queue_t * queue, const char item);
 
 // remove an item from the head of the queue.
-char queue_dequeue(queue_t * queue);
+__attribute__((always_inline))
+inline char queue_dequeue(queue_t * queue);
+
+// check if the queue is empty.
+inline char queue_is_empty(queue_t * queue)
+{
+	return (char)(queue->size == 0);
+}
+
+// check if the queue is full.
+inline char queue_is_full(queue_t * queue)
+{
+	return (char)(queue->size == QUEUE_SIZE);
+}
+
+// append an item to the tail of the queue.
+inline void queue_enqueue(queue_t * queue, const char item)
+{
+	queue->data[queue->tail] = item;
+	queue->tail = ADVANCE_INDEX(queue->tail);
+	queue->size++;
+}
+
+// remove an item from the head of the queue.
+inline char queue_dequeue(queue_t * queue)
+{
+	char item = queue->data[queue->head];
+	queue->head = ADVANCE_INDEX(queue->head);
+	queue->size--;
+	return item;
+}
 
 #endif				// QUEUE_H_
