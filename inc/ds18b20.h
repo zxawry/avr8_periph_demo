e4521d817edc711ebae219a4f1b7feebbe0fa071
Awry Zx <zxawry@tuta.io>
2023-07-04 12:57:57 +0100

    Tidy up C source and header files
    Changes to be committed:
    	modified:   inc/config.h
    	modified:   inc/ds1307.h
    	modified:   inc/ds18b20.h
    	modified:   inc/queue.h
    	modified:   inc/ssd1306.h
    	deleted:    inc/ssd1306_internal.h
    	modified:   inc/twi.h
    	modified:   inc/usart.h
    	modified:   src/convert.c
    	modified:   src/ds1307.c
    	modified:   src/ds18b20.c
    	modified:   src/main.c
    	modified:   src/ssd1306.c
    	modified:   src/twi.c

--- inc/ds18b20.h
+++ inc/ds18b20.h
@@ -9,6 +9,6 @@
 int ds18b20_init(void);
 int ds18b20_is_busy(void);
 int ds18b20_start_conversion(void);
-int ds18b20_get_temperature(char * str);
+int ds18b20_get_temperature(char *str);
 
 #endif
--- inc/twi.h
+++ inc/twi.h
@@ -3,21 +3,14 @@
 
 #include <stdint.h>
 
-#include "queue.h"
-
 #ifndef TWI_FREQ
-#define TWI_FREQ 100000UL
-#endif
-
-#ifndef TWI_BUFFER_LENGTH
-#define TWI_BUFFER_LENGTH 128
+#define TWI_FREQ (100000UL)
 #endif
 
 #define TWI_NOSTOP 0x01
 #define TWI_NOSTART 0x02
 
 void twi_init(void);
-
 int twi_transfer(uint8_t addr, uint8_t * data, uint8_t len, uint8_t flags);
 
 #define twi_write_bytes(addr, data, len, flags) \
